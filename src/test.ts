46b11d9d61f472b1b9daabc694315cb9b9e2647b
Michael Berger <m.gonser@icloud.com>
2022-12-06 07:01:16 +0100

    setup demo

--- src/test.ts
+++ src/test.ts
@@ -1,12 +1,12 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
 import 'zone.js/testing';
-import { getTestBed } from '@angular/core/testing';
+import { getTestBed, TestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
-
+import {MockStore} from '@ngrx/store/testing';
 declare const require: {
   context(path: string, deep?: boolean, filter?: RegExp): {
     <T>(id: string): T;
@@ -19,6 +19,10 @@ getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting(),
 );
+
+(jasmine.getEnv() as any).afterEach(() => {
+  TestBed.inject(MockStore)?.resetSelectors();
+})
 
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
